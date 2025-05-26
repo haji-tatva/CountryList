b9f8ffc6f88ee2e67496a624729ac471c4ff34d8
Hajivali Shaik <shaik.hajivali@tatvasoft.com>
2025-05-26 12:21:37 +0530

    initial commit

---
+++ CountryList/CountryList/App/CountryListApp.swift
@@ -0,0 +1,20 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by MACM23 on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
